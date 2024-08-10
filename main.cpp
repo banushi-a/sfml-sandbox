e97b1913759765d4848e58b83150c08c8896a26d
banushi-a <altonbanushi@icloud.com>
2024-08-10 16:03:51 -0400

    Refactor

--- main.cpp
+++ main.cpp
@@ -129,9 +129,6 @@ int main()
                     }
                     else if (data[i][j].material == WATER)
                     {
-                        // Fluid height function be Fh(x) where x is a fluid level [0, 9]
-                        // Fh(9) = 1, Fh(0) = 0, Fh(x) = (1/9)x
-                        double fluidHeightScaler = (1 / 9) * data[i][j].fluid_level;
                         sf::RectangleShape shape(sf::Vector2f(PIXEL_SIZE, PIXEL_SIZE));
                         shape.setFillColor(sf::Color(30, 192, 232));
                         shape.setPosition(sf::Vector2f(PIXEL_SIZE * j, PIXEL_SIZE * i));
@@ -140,9 +137,6 @@ int main()
                     }
                     else if (data[i][j].material == GAS)
                     {
-                        // Fluid height function be Fh(x) where x is a fluid level [0, 9]
-                        // Fh(9) = 1, Fh(0) = 0, Fh(x) = (1/9)x
-                        double fluidHeightScaler = (1 / 9) * data[i][j].fluid_level;
                         sf::RectangleShape shape(sf::Vector2f(PIXEL_SIZE, PIXEL_SIZE));
                         shape.setFillColor(sf::Color(89, 89, 89));
                         shape.setPosition(sf::Vector2f(PIXEL_SIZE * j, PIXEL_SIZE * i));
